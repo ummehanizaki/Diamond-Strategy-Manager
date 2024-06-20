74cb31baf00320a35a0fb54a7ba88f1721899dd4
ummehanizaki <ummehanizaki@gmail.com>
2024-06-21 03:07:59 +0530

    Changed addresses to mainnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,42 +8,42 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async () => {
-  const accounts = await ethers.getSigners()
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address)
+    console.log(account.address);
   }
-})
+});
 
 module.exports = {
   solidity: {
     compilers: [
-    { version: "0.8.20" },  // Existing compiler
-    { version: "0.8.0" }, // New compiler for specific files
-    ]
+      { version: "0.8.20" }, // Existing compiler
+      { version: "0.8.0" }, // New compiler for specific files
+    ],
   },
   networks: {
     hardhat: {
       forking: {
         url: SEPOLIA_API_KEY_URL,
-        accounts:[PRIVATE_KEY],
+        accounts: [PRIVATE_KEY],
         enabled: true,
         chainId: 12345,
-        blockNumber: 6104924
-      }
+        blockNumber: 20135534,
+      },
     },
     // sepolia: {
     //   url: SEPOLIA_API_KEY_URL,
     //   accounts: [PRIVATE_KEY],
     //   apiKey: process.env.ETHERSCAN_API_KEY
 
-    // },  
+    // },
   },
   etherscan: {
     apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY
-    }
+      sepolia: process.env.ETHERSCAN_API_KEY,
+    },
   },
   lockGasLimit: 200000000000,
   gasPrice: 10000000000,
--- scripts/constants.js
+++ scripts/constants.js
@@ -4,14 +4,14 @@ const FacetNames = ["DiamondLoupeFacet", "OwnershipFacet"];
 const amount = 10000000000000;
 
 const strategyNameAave = "Aave";
-const AaveWETH = "0xC558DBdd856501FCd9aaF1E62eae57A9F0629a3c";
-const AavePoolWETH = "0x6Ae43d3271ff6888e7Fc43Fd7321a503ff738951";
-const aWETH = "0x5b071b590a59395fE4025A0Ccc1FcC931AAc1830";
+const AaveWETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const AavePoolWETH = "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2";
+const aWETH = "0x4d5F47FA6A74757f35C14fD3a6Ef8E3C9BC514E8";
 
 const strategyNameCompound = "Compound";
-const CompoundWETH = "0x2D5ee574e710219a521449679A4A7f2B43f046ad";
-const CompoundPoolWETH = "0x2943ac1216979aD8dB76D9147F64E61adc126e96";
-const cWETH = "0x2943ac1216979aD8dB76D9147F64E61adc126e96";
+const CompoundWETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const CompoundPoolWETH = "0xA17581A9E3356d9A858b789D68B4d866e593aE94";
+const cWETH = "0xA17581A9E3356d9A858b789D68B4d866e593aE94";
 
 module.exports = {
   FacetNames,
